e0b8d8175c8d06a104570a0f582e25c33ccfaddd
GenericName1911 <genericname1911.official@gmail.com>
2025-09-08 22:58:25 +0530

    Upgrade pip version

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
 # Installs Pip
 import subprocess, sys
-subprocess.run([sys.executable, "-m", "pip", "install", "sc-compression==0.6.1"])
+subprocess.run([sys.executable, "-m", "pip", "install", "sc-compression==0.6.5"])
 input()
